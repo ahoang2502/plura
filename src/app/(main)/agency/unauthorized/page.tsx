1bdb92838c965ff0371fe4ff727bcaff49de3254
ahoang2502 <athoang252@gmail.com>
2024-02-24 23:50:17 +1100

    sidebar

---
+++ src/app/(main)/agency/unauthorized/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import Unauthorized from "@/components/unauthorized";
+
+const UnauthorizedPage = () => {
+	return <Unauthorized />;
+};
+
+export default UnauthorizedPage;
--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -300,3 +300,23 @@ export const upsertAgency = async (agency: Agency, price?: Plan) => {
 		console.log("[UPSERT_AGENCY]", error);
 	}
 };
+
+export const getNotificationAndUser = async (agencyId: string) => {
+	try {
+		const response = await db.notification.findMany({
+			where: {
+				agencyId,
+			},
+			include: {
+				User: true,
+			},
+			orderBy: {
+				createdAt: "desc",
+			},
+		});
+
+		return response;
+	} catch (error) {
+		console.log("[GET_NOTIFICATIONS]", error);
+	}
+};
