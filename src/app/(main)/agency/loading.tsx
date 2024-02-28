681a9d75653962c7bdc6a67371fc000ea833e68e
ahoang2502 <athoang252@gmail.com>
2024-02-28 14:22:29 +1100

    subaccount-main verification

---
+++ src/app/(main)/agency/loading.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Loading from "@/components/global/Loading";
+
+const LoadingAgencyPage = () => {
+	return (
+		<div className="h-screen w-screen flex justify-center items-center">
+			<Loading />
+		</div>
+	);
+};
+
+export default LoadingAgencyPage;
--- src/app/(main)/subaccount/page.tsx
+++ src/app/(main)/subaccount/page.tsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
 
-const SubAccountMainPage = () => {
-  return (
-    <div></div>
-  )
+import Unauthorized from "@/components/unauthorized";
+import { getAuthUserDetails, verifyAndAcceptInvitation } from "@/lib/queries";
+import UnauthorizedPage from "../agency/unauthorized/page";
+import { redirect } from "next/navigation";
+
+interface SubAccountMainPageProps {
+	searchParams: { state: string; code: string };
 }
 
-export default SubAccountMainPage
+const SubAccountMainPage = async ({
+	searchParams,
+}: SubAccountMainPageProps) => {
+	const agencyId = await verifyAndAcceptInvitation();
+	if (!agencyId) return <Unauthorized />;
+
+	const user = await getAuthUserDetails();
+	if (!user) return;
+
+	const getFirstSubaccountWithAccess = user.Permissions.find(
+		(permission) => permission.access === true
+	);
+
+	if (searchParams.state) {
+		const statePath = searchParams.state.split("___")[0];
+		const stateSubaccountId = searchParams.state.split("___")[1];
+
+		if (!stateSubaccountId) return <UnauthorizedPage />;
+
+		return redirect(
+			`/subaccount/${stateSubaccountId}/${statePath}?code=${searchParams.code}`
+		);
+	}
+
+	if (getFirstSubaccountWithAccess) {
+		return redirect(`/subaccount/${getFirstSubaccountWithAccess.subAccountId}`);
+	}
+
+	return <Unauthorized />;
+};
+
+export default SubAccountMainPage;
